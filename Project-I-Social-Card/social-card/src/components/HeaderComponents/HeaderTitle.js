305cecb9ffff0ccb2e616e8d959b003061f60d31
Ryan Ansberry <ansman92@gmail.com>
2019-05-29 18:31:45 -0700

    Setting up some basic layout

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Header.css';
+
+function HeaderTitle() {
+    return ( <div className="header-title-container">
+        <h1 className="header-title">Lambda School</h1>
+        <a className="handle">@LambdaSchool</a>
+        <span className="timestamp">29 may</span>
+    </div> );
+}
+
+export default HeaderTitle;
