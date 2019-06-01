9c45a993526de91f44a523b67b9f7fa75266b066
Ryan Ansberry <ansman92@gmail.com>
2019-05-31 18:11:56 -0700

    Layout for calculator established

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Display.css';
+
+const CalcDisplay = () => {
+    return (
+        <div className="calc-display">
+
+        </div>
+    );
+}
+
+export default CalcDisplay;
